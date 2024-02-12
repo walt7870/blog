d85d540a781e33161b138b0799ae34585ecef3cd
niu <niuwancheng@gamil.com>
2024-02-12 22:44:18 +0800

    deploy+

---
+++ assets/js/10.35ebdccf.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{599:function(t,e,n){"use strict";n.r(e);var s=n(17),l=Object(s.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[e("p",[this._v("识别聚合是认知潜在核心业务规则的过程，而定义聚合是在大家共识的基础上对核心业务规则的封装")])])}),[],!1,null,null,null);e.default=l.exports}}]);
